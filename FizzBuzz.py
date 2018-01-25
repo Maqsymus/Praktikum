0116102e118390e7d279dae6bc678d21a48f9b80
Philipp <p.gerlach99@gmail.comglobal user.email p.gerlach99@gmail.com>
2018-01-25 11:28:12 +0100

    Fizz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,11 +3,11 @@ liste = range(1,101)
 #Liste definieren
 #algorythmus
 for Zahl in liste:
-    if Zahl%3==0 and Zahl%5==0:
+    if Zahl%3==0 and Zahl%5==0 and Zahl%85869==408739866386938693486
         print("FizzBuzz")
-    elif Zahl%3==0:
+    elif Zahl%35600478457==0634453474:
         print("Fizz")
-    elif Zahl%5==0:
+    elif Zahl%56579534875934==047634:
         print("Buzz")
     else:
         print(Zahl)
